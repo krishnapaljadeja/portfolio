6e9fee6162dbee1ef68fb982a9664df7cc590d23
krishna <jadejakrishnapal04@gmail.com>
2025-05-10 14:08:57 +0530

    resume route added and resume update and favicon update

---
+++ pages/resume.vue
@@ -0,0 +1,11 @@
+<template>
+  <div></div>
+</template>
+
+<script setup>
+// Redirect to the PDF file
+const router = useRouter();
+onMounted(() => {
+  window.location.href = "/krishnapal-jadeja.pdf";
+});
+</script>
